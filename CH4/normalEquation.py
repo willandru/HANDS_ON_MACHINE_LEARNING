14382dad2d72a6e6260d7ac26928c10a88fb4506
willandru <willandru@hotmail.com>
2022-04-11 13:05:15 -0500

    Training Models: DONE

--- CH4/normalEquation.py
+++ CH4/normalEquation.py
@@ -10,3 +10,9 @@ plt.xlabel("$x_1$", fontsize=18)
 plt.ylabel("$y$", rotation=0, fontsize=18)
 plt.axis([0, 2, 0, 15])
 plt.show()
+
+#NORMAL EQUATION: 
+
+X_b = np.c_[np.ones((100,1)), X] 
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+print(theta_best)
